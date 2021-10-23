d720b615b7fbebe15fd2feb81c5eedc6bd5a872d
Giorgio Henrique Bazzo <giorgio@polisoftware.com.br>
2021-10-23 14:43:17 -0300

    commit inicial

---
+++ Project8.dpr
@@ -0,0 +1,14 @@
+program Project8;
+
+uses
+  Vcl.Forms,
+  Unit8 in 'Unit8.pas' {fmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfmMain, fmMain);
+  Application.Run;
+end.
